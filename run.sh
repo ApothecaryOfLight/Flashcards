f9b7de7e30b0b1eccd2c97e052176d5ca067398d
ApothecaryOfLight <ChangerOfWays@gmail.com>
2022-03-24 22:01:10 +0000

    Changed ip detection to public IP instead of private.

--- run.sh
+++ run.sh
@@ -2,11 +2,10 @@
 cd "${0%/*}"
 if [[ "$1" = "http" ]];
 then
-  IP=$(hostname -I | xargs)
-  echo "const ip = \"ws://${IP}:3001\";" > ./frontend/ip_file.js
+  echo "const ip = \"ws://${2}:3001\/\";" > ./frontend/ip_file.js
 elif [[ "$1" = "https" ]];
 then
-  echo "const ip = \"wss://triviacards.net:3001\";" > ./frontend/ip_file.js
+  echo "const ip = \"wss://triviacards.net:3001\/\";" > ./frontend/ip_file.js
 else
   echo "Command line argument:";
   echo "  run.sh dev";
@@ -16,4 +15,4 @@ else
   exit -1
 fi
 
-cd backend && screen -d -m -S Flashcards bash -c './run.sh "$1"'
+cd backend && screen -d -m -S Flashcards bash -c './run.sh "$1" "$2"'
