11955de1ba62dd87af9df09ee3d0797019e86911
ApothecaryOfLight <ChangerOfWays@gmail.com>
2022-04-03 17:52:38 +0000

    Run script now retains http/https switch on nodemon restart.

--- run.sh
+++ run.sh
@@ -3,9 +3,11 @@ cd "${0%/*}"
 if [[ "$1" = "http" ]];
 then
   echo "const ip = \"http://${2}:3001\/\";" > ./frontend/ip_file.js
+  cd backend && screen -d -m -S Flashcards bash -c './run.sh http "$2"'
 elif [[ "$1" = "https" ]];
 then
   echo "const ip = \"https://triviacards.net:3001\/\";" > ./frontend/ip_file.js
+  cd backend && screen -d -m -S Flashcards bash -c './run.sh https
 else
   echo "Command line argument:";
   echo "  run.sh dev";
@@ -15,4 +17,3 @@ else
   exit -1
 fi
 
-cd backend && screen -d -m -S Flashcards bash -c './run.sh "$1" "$2"'
