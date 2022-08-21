24b2c7c61987cb0a70f99a2678d937a0b9cd4947
ApothecaryOfLight <ChangerOfWays@gmail.com>
2022-08-21 20:10:58 +0000

    Removed deprecated tables from schema creation script.

--- create_schema.sql
+++ create_schema.sql
@@ -17,13 +17,7 @@ CREATE TABLE images_registry( card_id INT NOT NULL, set_id INT NOT NULL, global_
 CREATE TABLE error_log( error_id INT NOT NULL, PRIMARY KEY(error_id), timestamp DATETIME(6) NOT NULL, ip TINYTEXT, source VARCHAR(256), details TEXT );
 CREATE TABLE event_log( event_id INT NOT NULL, PRIMARY KEY(event_id), timestamp DATETIME(6) NOT NULL, ip TINYTEXT, code_source VARCHAR(256), details TEXT );
 
-CREATE TABLE 1_level_subjects ( name VARCHAR(150) NOT NULL, 1_level_subject_id INT NOT NULL PRIMARY KEY );
-CREATE TABLE 2_level_subjects ( name VARCHAR(150) NOT NULL, 2_level_subject_id INT NOT NULL PRIMARY KEY, member_of_1_level_subject_id INT NOT NULL, FOREIGN KEY (member_of_1_level_subject_id) REFERENCES 1_level_subjects(1_level_subject_id) ON DELETE CASCADE );
-CREATE TABLE 3_level_subjects ( name VARCHAR(150) NOT NULL, 3_level_subject_id INT NOT NULL PRIMARY KEY, member_of_2_level_subject_id INT NOT NULL, FOREIGN KEY (member_of_2_level_subject_id) REFERENCES 2_level_subjects(2_level_subject_id) ON DELETE CASCADE );
-CREATE TABLE 4_level_subjects ( name VARCHAR(150) NOT NULL, 4_level_subject_id INT NOT NULL PRIMARY KEY, member_of_3_level_subject_id INT NOT NULL, FOREIGN KEY (member_of_3_level_subject_id) REFERENCES 3_level_subjects(3_level_subject_id) ON DELETE CASCADE );
-
 CREATE TABLE subject_set_listing ( set_id INT NOT NULL, PRIMARY KEY (set_id), FOREIGN KEY (set_id) REFERENCES sets(set_id) ON DELETE CASCADE, 1_level_subject_id INT, FOREIGN KEY (1_level_subject_id) REFERENCES subject_level_listing(subject_id) ON DELETE CASCADE, 2_level_subject_id INT, FOREIGN KEY (2_level_subject_id) REFERENCES subject_level_listing(subject_id) ON DELETE CASCADE, 3_level_subject_id INT, FOREIGN KEY (3_level_subject_id) REFERENCES subject_level_listing(subject_id) ON DELETE CASCADE, 4_level_subject_id INT, FOREIGN KEY (4_level_subject_id) REFERENCES subject_level_listing(subject_id) ON DELETE CASCADE, INDEX subject_set_listing_lookup_index( 1_level_subject_id, 2_level_subject_id, 3_level_subject_id, 4_level_subject_id ) );
-
 CREATE TABLE subject_level_listing (name VARCHAR(150) NOT NULL, subject_id INT NOT NULL PRIMARY KEY, level TINYINT NOT NULL, parent_id INT, INDEX lookup_by_level_and_parent_index (parent_id, level), INDEX lookup_by_level (level), FOREIGN KEY (parent_id) REFERENCES subject_level_listing(subject_id) ON DELETE CASCADE);
 
 CREATE TABLE card_record( username_hash VARBINARY(64), card_id INT, FOREIGN KEY (card_id) REFERENCES cards(card_id) ON DELETE CASCADE, datestamp DATE, result INT );
