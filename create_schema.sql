4571b92e9b5839afde2af4e58250066c67ac77d7
ApothecaryOfLight <ChangerOfWays@gmail.com>
2022-08-22 14:01:05 +0000

    Rearranged schema creation script so that foreign keys in subject_set_listing would be able to reference fields in subject_level_listing.

--- create_schema.sql
+++ create_schema.sql
@@ -17,11 +17,10 @@ CREATE TABLE images_registry( card_id INT NOT NULL, set_id INT NOT NULL, global_
 CREATE TABLE error_log( error_id INT NOT NULL, PRIMARY KEY(error_id), timestamp DATETIME(6) NOT NULL, ip TINYTEXT, source VARCHAR(256), details TEXT );
 CREATE TABLE event_log( event_id INT NOT NULL, PRIMARY KEY(event_id), timestamp DATETIME(6) NOT NULL, ip TINYTEXT, code_source VARCHAR(256), details TEXT );
 
-CREATE TABLE subject_set_listing ( set_id INT NOT NULL, PRIMARY KEY (set_id), FOREIGN KEY (set_id) REFERENCES sets(set_id) ON DELETE CASCADE, 1_level_subject_id INT, FOREIGN KEY (1_level_subject_id) REFERENCES subject_level_listing(subject_id) ON DELETE CASCADE, 2_level_subject_id INT, FOREIGN KEY (2_level_subject_id) REFERENCES subject_level_listing(subject_id) ON DELETE CASCADE, 3_level_subject_id INT, FOREIGN KEY (3_level_subject_id) REFERENCES subject_level_listing(subject_id) ON DELETE CASCADE, 4_level_subject_id INT, FOREIGN KEY (4_level_subject_id) REFERENCES subject_level_listing(subject_id) ON DELETE CASCADE, INDEX subject_set_listing_lookup_index( 1_level_subject_id, 2_level_subject_id, 3_level_subject_id, 4_level_subject_id ) );
 CREATE TABLE subject_level_listing (name VARCHAR(150) NOT NULL, subject_id INT NOT NULL PRIMARY KEY, level TINYINT NOT NULL, parent_id INT, INDEX lookup_by_level_and_parent_index (parent_id, level), INDEX lookup_by_level (level), FOREIGN KEY (parent_id) REFERENCES subject_level_listing(subject_id) ON DELETE CASCADE);
+CREATE TABLE subject_set_listing ( set_id INT NOT NULL, PRIMARY KEY (set_id), FOREIGN KEY (set_id) REFERENCES sets(set_id) ON DELETE CASCADE, 1_level_subject_id INT, FOREIGN KEY (1_level_subject_id) REFERENCES subject_level_listing(subject_id) ON DELETE CASCADE, 2_level_subject_id INT, FOREIGN KEY (2_level_subject_id) REFERENCES subject_level_listing(subject_id) ON DELETE CASCADE, 3_level_subject_id INT, FOREIGN KEY (3_level_subject_id) REFERENCES subject_level_listing(subject_id) ON DELETE CASCADE, 4_level_subject_id INT, FOREIGN KEY (4_level_subject_id) REFERENCES subject_level_listing(subject_id) ON DELETE CASCADE, INDEX subject_set_listing_lookup_index( 1_level_subject_id, 2_level_subject_id, 3_level_subject_id, 4_level_subject_id ) );
 
 CREATE TABLE card_record( username_hash VARBINARY(64), card_id INT, FOREIGN KEY (card_id) REFERENCES cards(card_id) ON DELETE CASCADE, datestamp DATE, result INT );
-
 
 CREATE TABLE sequence_last( sequence_id TINYINT, last BIGINT NOT NULL );
 CREATE TABLE sequence_retired( sequence_id TINYINT, retired_id BIGINT NOT NULL );
@@ -32,7 +31,6 @@ INSERT INTO sequence_last (sequence_id,last) VALUES (3,0);
 INSERT INTO sequence_last (sequence_id,last) VALUES (4,0);
 INSERT INTO sequence_last (sequence_id,last) VALUES (5,0);
 INSERT INTO sequence_last (sequence_id,last) VALUES (6,0);
-
 
 DELIMITER %%
 CREATE FUNCTION Flashcards.generate_new_id( in_sequence_id TINYINT )
